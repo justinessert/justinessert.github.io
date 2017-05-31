3e3304d18e4ba8bcf58f41283fe1c4adb7480299
Justin Essert <justinkessert@gmail.com>
2017-05-31 17:05:12 -0500

    added static files

---
+++ app/app.js
@@ -0,0 +1,3 @@
+angular.module('myapp', ['ngRoute', 'mainCtrl', 'duScroll'])
+	.value('duScrollDuration', 1000)
+	.value('duScrollOffset', 50);
---
+++ app/controllers/mainCtrl.js
@@ -0,0 +1,4 @@
+angular.module('mainCtrl', ['ngRoute'])
+	.controller('mainController', function() {
+
+	});
